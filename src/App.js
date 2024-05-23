9ebb8f542e05e8e1ea8c22bbcbaffdad0fe83aa3
hkowrada <harish.kowrada@gmail.com>
2024-05-23 08:35:49 +0200

    modified reservations

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import React from "react";
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './App.css';
 
-import {
-  AboutUs,
-  Chef,
-  FindUs,
-  Footer,
-  Gallery,
-  Header,
-  Intro,
-  Laurels,
-  SpecialMenu,
-} from "./container";
-import { Navbar } from "./components";
-import "./App.css";
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const Menu = lazy(() => import('./pages/Menu'));
+const Reservation = lazy(() => import('./pages/Reservation'));
 
-import Home from "./pages/Home";
-import Menu from "./pages/Menu";
-import About from "./pages/About";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Reservation from "./pages/Reservation";
-
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" Component={Home} />
-      <Route path="/menu" Component={Menu} />
-      <Route path="/about" Component={About} />
-      <Route path="/reservation" Component={Reservation} />
-    </Routes>
-  </BrowserRouter>
-);
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/reservation" component={Reservation} />
+        </Switch>
+      </Suspense>
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,30 +1,12 @@
-import React from "react";
-// import { ReservationForm } from "../components/Form/ResevationForm";
-
-import {
-  AboutUs,
-  Chef,
-  FindUs,
-  Footer,
-  Gallery,
-  Header,
-  Intro,
-  Laurels,
-  SpecialMenu,
-} from "../container";
-import { Navbar } from "../components";
-import "../App.css";
-// import Calendar from "react-calendar";
-import { CalendarCustom } from "../components";
-import ReservationForm from "../components/Form/ResevationForm";
+import React from 'react';
+import { Navbar, Footer } from '../components';
+import ReservationForm from '../components/Form/ReservationForm';
+import '../App.css';  // Ensure that styles are applied
 
 const Reservation = () => (
   <div>
     <Navbar />
-    {/* <Header /> */}
-    {/* <CalendarCustom /> */}
     <ReservationForm />
-
     <Footer />
   </div>
 );
